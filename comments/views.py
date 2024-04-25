2b05c2f551db59c4e1b6e0df3847cdc05ec54d96
Kim Moberg <kimskogfelter@outlook.com>
2024-04-25 09:31:57 +0000

    CommentList and CommentDetail generic views

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from djangorest.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentDetailSerializer, CommentSerializer
 
-# Create your views here.
+
+
+class CommentList(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
+    
