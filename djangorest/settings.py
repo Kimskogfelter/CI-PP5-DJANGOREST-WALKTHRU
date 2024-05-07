5bf62e66490b4d1af381f8a9c464a98c62b4e28b
Kim Moberg <kimskogfelter@outlook.com>
2024-05-07 07:15:48 +0000

    jwt aut cookie and client origin changed in settings

--- djangorest/settings.py
+++ djangorest/settings.py
@@ -47,7 +47,7 @@ if 'DEV' not in os.environ:
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
-JWT_AUTH_COOKIE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
@@ -110,17 +110,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
-
-
-CORS_ALLOWED_ORIGINS = [
-    "https://3000-kimskogfelt-cipp5moment-gn7qyks86r9.ws-eu111.gitpod.io",
-
-]
 
 CORS_ALLOW_CREDENTIALS = True
 
